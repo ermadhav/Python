8b134bd8b0e75ed7bb44616d73286a495effea4b
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-05 23:18:00 +0530

    Updated

--- External_Modules/req.py
+++ External_Modules/req.py
@@ -1,5 +1,8 @@
-import requests
-r = requests.get('https://api.github.com/users/ermadhav')
-print(r.text)
-with open("ermadhav.txt", "w") as f:
-    f.write(r.text)
+# import requests
+# r = requests.get('https://api.github.com/users/ermadhav')
+# print(r.text)
+# with open("ermadhav.txt", "w") as f:
+#     f.write(r.text)
+
+import re
+text = "Madhav is a genius Boy, who loves space and coding"
