9e74254eb3664b81bc565cacff491282d7918624
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-06 22:35:00 +0530

    Updated

--- projects/main.py
+++ projects/main.py
@@ -1,18 +1,19 @@
-try:
-    a = int(input("Enter the first num: "))
-    b = int(input("Enter the first num: "))
+# try:
+#     a = int(input("Enter the first num: "))
+#     print("What kind of operation you want to perform : +,-,*,/ ")
+#     o = input("Enter operation: ")
+#     b = int(input("Enter the first num: "))
 
-    print("What kind of operation you want to perform : +,-,*,/ ")
-    o = input("Enter operation: ")
-    match o: 
-        case "+":
-            print(f"result = {a+b}")
-        case "-":
-            print(f"result = {a-b}")
-        case "*":
-            print(f"result = {a*b}")
-        case "/":
-            print(f"result = {a/b}")    
+    
+#     match o: 
+#         case "+":
+#             print(f"result = {a+b}")
+#         case "-":
+#             print(f"result = {a-b}")
+#         case "*":
+#             print(f"result = {a*b}")
+#         case "/":
+#             print(f"result = {a/b}")    
 
-except Exception as e:
-    print("enter a valid value of a and b")        
+# except Exception as e:
+#     print("enter a valid value of a and b")        
