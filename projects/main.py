5343f3183b5b4e8e70ab61644f63fd94537d7215
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-09 23:37:49 +0530

    Updated

--- projects/main.py
+++ projects/main.py
@@ -103,18 +103,18 @@
 
 # Project :- 5
 
-import os
+# import os
 
-def arrange_files(files, ext):
-    files_with_ext = [file for file in files if file.endswith(ext)]
-    print(files_with_ext)
+# def arrange_files(files, ext):
+#     files_with_ext = [file for file in files if file.endswith(ext)]
+#     print(files_with_ext)
 
-    if not(os.path.exists("images")):
-        os.mkdir("images")
+#     if not(os.path.exists("images")):
+#         os.mkdir("images")
     
-    for i,file in enumerate(files_with_ext):
-        os.rename(file, f"images/photo-{i+1}{ext}")
+#     for i,file in enumerate(files_with_ext):
+#         os.rename(file, f"images/photo-{i+1}{ext}")
 
-if __name__ == "__main__":
-    files = os.listdir()
-    arrange_files(files, ".jpg")
+# if __name__ == "__main__":
+#     files = os.listdir()
+#     arrange_files(files, ".jpg")
