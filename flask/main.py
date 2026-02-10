3775dd9002da18eebbc384e2faab4e4fab7ccbce
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-10 22:03:18 +0530

    updated

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Cosmo Coder!</p>"
-
+    return render_template("index.html")
 app.run(debug = True)
