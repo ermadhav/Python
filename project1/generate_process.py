fd36f5f8bca62e1ca4a23e6fec6219cb1dd1fb2f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-17 23:37:58 +0530

    updated

--- project1/generate_process.py
+++ project1/generate_process.py
@@ -3,16 +3,16 @@ import os
 def text_to_audio(folder):
     pass
 
-def create_reel():
+def create_reel(folder):
     pass
 
 if __name__ == "__main__":
     with open("done.txt", "r") as f:
-        done_folders = f.readlines()
+        done_folders = [line.strip() for line in f.readlines()]
 
     folders = os.listdir("user_uploads")
+
     for folder in folders:
-        if(folder not in done_folders):
+        if folder not in done_folders:
             text_to_audio(folder)
             create_reel(folder)
-    
