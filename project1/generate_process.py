ef5a763161107989d67715e8daeb770e0424bcd4
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-17 23:27:23 +0530

    updated

--- project1/generate_process.py
+++ project1/generate_process.py
@@ -1,7 +1,10 @@
 def text_to_audio(folder):
     pass
+
 def create_reel():
     pass
 
 if __name__ == "__main__":
-    
+    folder = "588....."
+    text_to_audio(folder)
+    create_reel(folder)
