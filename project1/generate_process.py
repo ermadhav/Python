89fccff447ed03036ecfc2f21aa8736bd49b8c67
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-17 23:41:23 +0530

    updated

--- project1/generate_process.py
+++ project1/generate_process.py
@@ -1,18 +1,22 @@
-import os
+# This file looks for new folders inside user uploads and converts them to reel if they are not already converted
+import os 
+
 
 def text_to_audio(folder):
-    pass
+    print("TTA - ", folder)
 
 def create_reel(folder):
-    pass
+    print("CR - ", folder)
 
 if __name__ == "__main__":
     with open("done.txt", "r") as f:
-        done_folders = [line.strip() for line in f.readlines()]
+        done_folders = f.readlines()
 
-    folders = os.listdir("user_uploads")
-
+    done_folders = [f.strip() for f in done_folders]
+    folders = os.listdir("user_uploads") 
     for folder in folders:
-        if folder not in done_folders:
-            text_to_audio(folder)
-            create_reel(folder)
+        if(folder not in done_folders): 
+            text_to_audio(folder) # Generate the audio.mp3 from desc.txt
+            create_reel(folder) # Convert the images and audio.mp3 inside the folder to a reel
+            with open("done.txt", "a") as f:
+                f.write(folder + "\n")
