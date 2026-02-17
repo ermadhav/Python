dd3e4d51f8b8513e82985721e6903f196eebe949
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-17 23:29:04 +0530

    updated

--- project1/generate_process.py
+++ project1/generate_process.py
@@ -5,6 +5,10 @@ def create_reel():
     pass
 
 if __name__ == "__main__":
+    with open("done.txt", "r") as f:
+        folder = f.readline()
+
+    folders = os.listdir("user_uploads")
     folder = "588....."
     text_to_audio(folder)
     create_reel(folder)
