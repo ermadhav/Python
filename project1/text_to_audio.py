d0352dbb8f6df3f5b94e3a1100b9cc0fdbca463b
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-03-20 00:29:21 +0530

    Updated

--- project1/text_to_audio.py
+++ project1/text_to_audio.py
@@ -12,13 +12,11 @@ client = ElevenLabs(
 
 
 def text_to_speech_file(text: str, folder: str) -> str:
-    # Calling the text_to_speech conversion API with detailed parameters
     response = client.text_to_speech.convert(
-        voice_id="pNInz6obpgDQGcFmaJgB", # Adam pre-made voice
+        voice_id="pNInz6obpgDQGcFmaJgB", 
         output_format="mp3_22050_32",
         text=text,
-        model_id="eleven_turbo_v2_5", # use the turbo model for low latency
-        # Optional voice settings that allow you to customize the output
+        model_id="eleven_turbo_v2_5", 
         voice_settings=VoiceSettings(
             stability=0.0,
             similarity_boost=1.0,
@@ -28,13 +26,9 @@ def text_to_speech_file(text: str, folder: str) -> str:
         ),
     )
 
-    # uncomment the line below to play the audio back
-    # play(response)
 
-    # Generating a unique file name for the output MP3 file
     save_file_path = os.path.join(f"user_uploads/{folder}", "audio.mp3")
 
-    # Writing the audio to a file
     with open(save_file_path, "wb") as f:
         for chunk in response:
             if chunk:
@@ -42,7 +36,6 @@ def text_to_speech_file(text: str, folder: str) -> str:
 
     print(f"{save_file_path}: A new audio file was saved successfully!")
 
-    # Return the path of the saved audio file
     return save_file_path
 
 
